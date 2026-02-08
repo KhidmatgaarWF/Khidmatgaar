0031b148f2623c0983bf868858b17539142dae6a
KhidmatgaarWF <khidmatgaarwf@gmail.com>
2026-02-08 12:51:51 +0500

    update

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("Helping Hands website loaded");
+
+// NAVBAR TOGGLE
+const toggleBtn = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if (toggleBtn && navLinks) {
+  toggleBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
+
+
